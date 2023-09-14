f88e1661244d22e02facc9678a51e334e6017d19
peekors <peekors@gmail.com>
2023-09-14 13:04:36 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,6 +63,9 @@ const TemplateWrapper = ({ children }) => (
             <li>
               <Link to="/about">About</Link>
             </li>
+            <li>
+              <Link to="/about">Subs</Link>
+            </li>
           </ul>
           <p className="sidebar__social">
             {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
